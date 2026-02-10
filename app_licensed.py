bcee139c04ce017e7e9b69f875c7ec419ae1b7fa
Rafael <rafael.araujobazilio@gmail.com>
2026-02-09 22:49:35 -0300

    feat: aplicar mesmas otimizações (1080p + blocos grid) no processamento em lote

--- app_licensed.py
+++ app_licensed.py
@@ -675,10 +675,35 @@ def generate_sketch_video_single(
         
         img_ht, img_wd = img.shape[0], img.shape[1]
         
-        # Ajustar resolução
+        # Ajustar resolução (limitar a 1920x1080 máximo)
         aspect_ratio = img_wd / img_ht
-        target_ht = find_nearest_res(img_ht)
-        target_wd = find_nearest_res(int(target_ht * aspect_ratio))
+        MAX_HEIGHT = 1080
+        MAX_WIDTH = 1920
+        
+        if img_ht > MAX_HEIGHT or img_wd > MAX_WIDTH:
+            if img_wd / MAX_WIDTH > img_ht / MAX_HEIGHT:
+                target_wd = MAX_WIDTH
+                target_ht = int(target_wd / aspect_ratio)
+            else:
+                target_ht = MAX_HEIGHT
+                target_wd = int(target_ht * aspect_ratio)
+        else:
+            target_ht = img_ht
+            target_wd = img_wd
+        
+        # GARANTIR que dimensões sejam divisíveis pelo split_len
+        target_wd = (target_wd // split_len) * split_len
+        target_ht = (target_ht // split_len) * split_len
+        
+        # Garantir dimensões mínimas
+        min_dim = split_len * 2
+        target_wd = max(target_wd, min_dim)
+        target_ht = max(target_ht, min_dim)
+        
+        # Ajustar para valores pares
+        target_ht = target_ht if target_ht % 2 == 0 else target_ht - 1
+        target_wd = target_wd if target_wd % 2 == 0 else target_wd - 1
+        
         img = cv2.resize(img, (target_wd, target_ht))
         
         # Processar imagem
@@ -757,97 +782,86 @@ def generate_sketch_video_single(
         
         # === FASE 2: COLORIZAÇÃO POR REGIÕES (se modo selecionado) ===
         if draw_mode == "Contornos + Colorização":
-            # Inverter threshold para encontrar regiões fechadas
             img_thresh_inv = cv2.bitwise_not(img_thresh)
-            
-            # Dilatar levemente os contornos para fechar gaps pequenos
             kernel = np.ones((3, 3), np.uint8)
             img_thresh_dilated = cv2.dilate(img_thresh_inv, kernel, iterations=1)
             img_thresh_for_regions = cv2.bitwise_not(img_thresh_dilated)
             
-            # Encontrar regiões conectadas
             num_labels, labels = cv2.connectedComponents(img_thresh_for_regions)
             
-            # Calcular info de cada região
             region_info = []
             for label_id in range(1, num_labels):
                 region_mask = (labels == label_id)
                 region_size = np.sum(region_mask)
                 
-                if region_size < 20:
+                if region_size < 50:
                     continue
                 
                 ys, xs = np.where(region_mask)
-                cy, cx = int(np.mean(ys)), int(np.mean(xs))
+                if len(ys) == 0:
+                    continue
+                
+                mean_color = np.mean(img[ys, xs], axis=0)
+                if np.all(mean_color > 245):
+                    continue
                 
                 region_info.append({
                     'label_id': label_id,
                     'size': region_size,
-                    'cx': cx,
-                    'cy': cy,
-                    'mask': region_mask
+                    'ys': ys,
+                    'xs': xs
                 })
             
-            # Ordenar: regiões menores primeiro (objetos), maiores por último (fundo)
             region_info.sort(key=lambda r: r['size'])
             
             color_skip = max(1, skip_rate // 2)
-            color_counter = 0
+            block_counter = 0
             
             for region in region_info:
-                region_mask = region['mask']
-                ys, xs = np.where(region_mask)
+                ys, xs = region['ys'], region['xs']
                 
-                if len(ys) == 0:
+                # Agrupar pixels em blocos de grid usando NumPy (vetorizado)
+                grid_rows = ys // split_len
+                grid_cols = xs // split_len
+                grid_keys_arr = grid_rows * 10000 + grid_cols
+                unique_keys = np.unique(grid_keys_arr)
+                
+                blocks = []
+                for key in unique_keys:
+                    mask = grid_keys_arr == key
+                    blocks.append((ys[mask], xs[mask], int(key // 10000), int(key % 10000)))
+                
+                if len(blocks) == 0:
                     continue
                 
-                # Pular regiões brancas/quase brancas (canvas já é branco)
-                mean_color = np.mean(img[ys, xs], axis=0)
-                if np.all(mean_color > 240):
-                    continue
-                
-                # Agrupar pixels em mini-blocos baseados no grid
-                pixel_grids = {}
-                for py, px in zip(ys, xs):
-                    grid_row = py // split_len
-                    grid_col = px // split_len
-                    key = (grid_row, grid_col)
-                    if key not in pixel_grids:
-                        pixel_grids[key] = []
-                    pixel_grids[key].append((py, px))
-                
-                # Ordenar grids por distância euclidiana
-                grid_keys = list(pixel_grids.keys())
-                if len(grid_keys) == 0:
-                    continue
-                
-                grid_coords = np.array(grid_keys)
-                ordered_grids = []
-                current_idx = 0
-                remaining = list(range(len(grid_coords)))
+                # Ordenar blocos por proximidade
+                ordered_blocks = [blocks[0]]
+                remaining = list(range(1, len(blocks)))
+                current_r, current_c = blocks[0][2], blocks[0][3]
                 
                 while remaining:
-                    ordered_grids.append(remaining[current_idx])
-                    current_pos = grid_coords[remaining[current_idx]].copy()
-                    remaining.pop(current_idx)
+                    best_idx = 0
+                    best_dist = float('inf')
+                    for i, idx in enumerate(remaining):
+                        dr = blocks[idx][2] - current_r
+                        dc = blocks[idx][3] - current_c
+                        dist = dr * dr + dc * dc
+                        if dist < best_dist:
+                            best_dist = dist
+                            best_idx = i
                     
-                    if remaining:
-                        remaining_coords = grid_coords[remaining]
-                        dists = euc_dist(remaining_coords, current_pos)
-                        current_idx = np.argmin(dists)
+                    chosen = remaining.pop(best_idx)
+                    ordered_blocks.append(blocks[chosen])
+                    current_r, current_c = blocks[chosen][2], blocks[chosen][3]
                 
-                # Pintar cada mini-bloco desta região
-                for grid_idx in ordered_grids:
-                    grid_key = grid_keys[grid_idx]
-                    pixels = pixel_grids[grid_key]
+                # Pintar bloco por bloco com animação
+                for block_ys, block_xs, gr_row, gr_col in ordered_blocks:
+                    drawn_frame[block_ys, block_xs] = img[block_ys, block_xs]
                     
-                    for py, px in pixels:
-                        drawn_frame[py, px] = img[py, px]
-                    
-                    color_counter += 1
-                    if color_counter % color_skip == 0:
-                        hx = min(grid_key[1] * split_len + split_len // 2, target_wd - 1)
-                        hy = min(grid_key[0] * split_len + split_len // 2, target_ht - 1)
+                    block_counter += 1
+                    if block_counter % color_skip == 0:
+                        hx = min(gr_col * split_len + split_len // 2, target_wd - 1)
+                        hy = min(gr_row * split_len + split_len // 2, target_ht - 1)
                         
                         drawn_frame_with_hand = draw_hand_on_img(
                             drawn_frame.copy(), hand.copy(), hx, hy,
